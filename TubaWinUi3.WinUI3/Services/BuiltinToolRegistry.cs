4300fc3e539db71f57da42977f0f2a6991ba477a
luolangaga <2667210109@qq.com>
2026-08-18 02:05:35 +0800

    精简工具集

--- TubaWinUi3.WinUI3/Services/BuiltinToolRegistry.cs
+++ TubaWinUi3.WinUI3/Services/BuiltinToolRegistry.cs
@@ -32,8 +32,6 @@ public static class BuiltinToolRegistry
         Register(new ContextMenuMgrTool());
         Register(new HardwareSpooferTool());
         Register(new NetworkAdapterProxyTool());
-        Register(new CommonSoftwareTool());
-        Register(new WindowsOptimizeTool());
         Register(new UniGetUITool());
         Register(new OptimizerDuckTool());
         Register(new AiAssistantTool());
